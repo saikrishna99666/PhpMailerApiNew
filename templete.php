2b6d510fe4b9f6140cfd658769c523ddc988cb69
saikrishna <33865376+saikrishna99666@users.noreply.github.com>
2022-02-02 15:20:34 +0530

    Add files via upload

---
+++ templete.php
@@ -0,0 +1,10 @@
+<div>
+    <p> Thank you for contacting us we will get backto you soon. your details are as follows:</p>
+    <p>
+        Subject: %subject% <br>
+        Message: %message%<br>
+        Email: %email%<br/>
+        Name: %Name%<br/>
+        query: %query%<br/>
+    </p>
+</div>
